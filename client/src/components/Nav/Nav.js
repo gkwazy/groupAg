c026193d437731f415013607e382aadde08c6a1c
gkwazy <no-replay>
2018-10-18 16:21:59 -0600

    video

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,9 +1,10 @@
 import React from "react";
+import logo from "../../pic/logo.png"
 
 const Nav = ({ children }) => (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a className="navbar-brand" href="/">
-            {children}
+        <a className="navbar-brand" href="#">
+            <img src={logo} width="200" height="80" alt="ops" />
         </a>
     </nav>
 );
